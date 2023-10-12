d9b1ca26f3b65ba2a8db2a829149644f5a796236
acf-patrick <miharisoap@gmail.com>
2023-10-12 21:02:35 +0300

    update

--- src/schema.rs
+++ src/schema.rs
@@ -1,58 +1,26 @@
 use juniper::*;
 
 #[derive(GraphQLObject)]
-#[graphql(description = "A Todo")]
-struct Todo {
+#[graphql(description = "A member of a team")]
+struct Member {
     id: i32,
-    title: String,
-    description: String,
-    completed: bool,
+    name: String,
 }
 
-#[derive(GraphQLInputObject)]
-#[graphql(description = "A Todo to insert")]
-struct NewTodo {
-    title: String,
-    description: String,
-    completed: bool,
-}
-
-struct Query;
+pub struct QueryRoot;
 
 #[graphql_object]
-impl Query {
-    fn apiVersion() -> &'static str {
-        "1.0"
-    }
-
-    fn todos() -> Vec<Todo> {
+impl QueryRoot {
+    fn members() -> Vec<Member> {
         vec![
-            Todo {
+            Member {
                 id: 1,
-                title: "Watching Basketball".to_owned(),
-                description: "Watching the NBA finals".to_owned(),
-                completed: false,
+                name: "Link".to_owned()
             },
-            Todo {
+            Member {
                 id: 2,
-                title: "Watching Football".to_owned(),
-                description: "Watching the NFL".to_owned(),
-                completed: false,
-            },
+                name: "Mario".to_owned()
+            }
         ]
-    }
-}
-
-struct Mutation;
-
-#[graphql_object]
-impl Mutation {
-    fn createTodo(new_todo: NewTodo) -> Todo {
-        Todo {
-            id: 1,
-            title: new_todo.title,
-            description: new_todo.description,
-            completed: new_todo.completed,
-        }
     }
 }
